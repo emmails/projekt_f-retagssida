d42a20f117a9b88ddd4d34f34325e90eb5ef2df9
Emma Ilstam <emma.ilstam@gmail.com>
2022-12-13 16:49:30 +0100

    Almost final

--- js/index.js
+++ js/index.js
@@ -36,27 +36,3 @@ if(window.matchMedia("(min-width:576px)").matches) {
 else {
     $(carouselExampleControls).addClass("slide");
 }
-
-
-
-
-
-
-/* //Width of the carousel
-let carouselWidth = document.getElementsByClassName("carousel-inner").offsetWidth;; 
-
-//Width of a single card 
-let cardWidth = document.getElementsByClassName("carousel-item").clientWidth;;
-
-let nextButton = document.getElementsByClassName("carousel-control-next");
-
-//Set scroll position
-let scrollPosition = 0;
-
-//Function for clicking on the next button
-nextButton.addEventListener("click", () => {
-    console.log("next");
-    scrollPosition = scrollPosition + cardWidth;
-    //600 stands for duration 600 milliseconds
-    $(".carousel-inner").animate({scrollLeft: scrollPosition}, 600)
-}) */
